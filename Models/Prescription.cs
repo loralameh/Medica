e6408d88bf9f3b624be6fc9dd1bb4a3e80875dfc
lora lameh <loralameh@gmail.com>
2021-09-11 23:14:43 +0300

    add Prescription Model

---
+++ Models/Prescription.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Medica.Models
+{
+    public class Prescription
+    {
+        public string DrFirstName;
+        public string DrLastName;
+        public string PatientFirstName;
+        public string PatientLastName;
+        public string DiseaseDiscription;
+        public string MedicinName;
+        public DateTime StartDate;
+        public DateTime EndDate;
+        public int PillsPerDay;
+        public string Instructions;
+
+
+
+    }
+}
