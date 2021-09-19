8a8c3cb9f6394c28917e7faec5b0100d5f6b4e31
lora lameh <loralameh@gmail.com>
2021-09-19 15:12:45 +0300

    add UserRoleViewModel

---
+++ ViewModels/UserRoleViewMode.cs
@@ -0,0 +1,23 @@
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Medica.ViewModels;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Policy;
+using Microsoft.AspNetCore.Identity;
+
+namespace Medica.ViewModels
+{
+    public class UserRoleViewModel
+    {
+
+    public string UserId { get; set; }
+    public string UserName { get; set; }
+    public bool IsSelected { get; set; }
+    }
+}
