b58276855c88ea63d53e781d0cbdba8c59884a20
lora lameh <loralameh@gmail.com>
2021-09-19 03:16:12 +0300

    edit AdministrationController

--- Controllers/AdministrationController.cs
+++ Controllers/AdministrationController.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Medica.Data;
 using Medica.Models;
+using Medica.ViewModels;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -19,6 +22,36 @@ namespace Medica.Controllers
             this.roleManager = roleManager;
 
         }
+
+        [HttpGet]
+        public IActionResult CreateRole(){
+            return View();
+        }
+
+       [HttpPost]
+         public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityRole identityRole = new IdentityRole{
+                        Name=model.RoleName
+                    };
+                IdentityResult result=await roleManager.CreateAsync(identityRole);
+                
+                if(result.Succeeded)
+                    {
+                    return Redirect("~/Home/Index");
+
+                    }
+               else{
+                   return NotFound();
+                    }
+            }
+            return View(model);
+
+        }
+
+        
 
     }
 
